4edcf0a112ae889b2fff029d4785b5ade31e5b71
Aimeast <lixd3389@gmail.com>
2013-03-31 15:03:42 +0800

    Add config entry

---
+++ Bonobo.Git.Server/Bonobo.Git.Server/Configs/UserConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Serialization;
+
+namespace Bonobo.Git.Server.Configs
+{
+    [XmlRootAttribute(ElementName = "Configuration", IsNullable = false)]
+    public class UserConfiguration : ConfigEntry<UserConfiguration>
+    {
+        public bool AllowAnonymousPush { get; set; }
+        public string Repositories { get; set; }
+        public bool AllowUserRepositoryCreation { get; set; }
+        public bool AllowAnonymousRegistration { get; set; }
+    }
+}
