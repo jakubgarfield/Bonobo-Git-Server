70443ae2f01e81eb2a15c5d5ba9d785a2ab36f03
kfarnung <kfarnung@outlook.com>
2014-03-24 23:40:36 -0700

    CssRewriteUrlTransform doesn't account for virtual paths
    When processing CSS files in a bundle, the CssRewriteUrlTransform class
    doesn't account for virtual paths, instead assuming that the application
    is located at the domain root (assumes http://foobar.com/, but it's
    actually http://foobar.com/Git).  This change implements a new wrapper
    class that ensures that virtual directories are taken into account.
    
    Since it just modifies the path and then passes it to the original
    class, this should be low risk.  If the bug is ever fixed in the
    framework there will be a regression, but the bug will leave users no
    worse off than they are now.
    
    Found solution from http://aspnetoptimization.codeplex.com/workitem/83
    via
    http://stackoverflow.com/questions/19765238/cssrewriteurltransform-with-or-without-virtual-directory
    
    Tested the change using IIS Express on my local machine (no virtual path
    required) and on my production IIS server (using a virtual path).  Both
    tests were successful.

---
+++ Bonobo.Git.Server/Helpers/CssRewriteUrlTransformWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Bonobo.Git.Server.Helpers
+{
+    public class CssRewriteUrlTransformWrapper : IItemTransform
+    {
+        public string Process(string includedVirtualPath, string input)
+        {
+            return new CssRewriteUrlTransform().Process(
+                "~" + VirtualPathUtility.ToAbsolute(includedVirtualPath),
+                input);
+        }
+    }
+}
