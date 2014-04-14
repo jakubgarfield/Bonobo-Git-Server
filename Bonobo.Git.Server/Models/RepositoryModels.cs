06ea9e04ab26f8d59daf7b12c6259f8c2670610f
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2014-04-15 06:40:46 +1200

    Date string added to model for better JSON cooperation

--- Bonobo.Git.Server/Models/RepositoryModels.cs
+++ Bonobo.Git.Server/Models/RepositoryModels.cs
@@ -67,6 +67,7 @@ namespace Bonobo.Git.Server.Models
 
         [Display(ResourceType = typeof(Resources), Name = "Repository_Tree_CommitDate")]
         public DateTime? CommitDate { get; set; }
+        public string CommitDateString { get { return CommitDate.ToString(); } }
 
         [Display(ResourceType = typeof(Resources), Name = "Repository_Tree_Author")]
         public string Author { get; set; }
