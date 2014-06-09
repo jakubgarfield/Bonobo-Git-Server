34d2b2f24a6879ce0415f4b2d29c360f04b4e8e1
Sebastiaan Lubbers <github@duikmeester.nl>
2014-06-09 23:09:08 +0200

    Upgraded to Entity Framework 6 this closes #4
    Should open new ticket to change the modeling to true Entity Framework methodology

--- Bonobo.Git.Server/Data/EFRepositoryRepository.cs
+++ Bonobo.Git.Server/Data/EFRepositoryRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core;
 using System.Linq;
 using Bonobo.Git.Server.Models;
 using System.Data;
--- Bonobo.Git.Server/Data/EFTeamRepository.cs
+++ Bonobo.Git.Server/Data/EFTeamRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Data;
+using System.Data.Entity.Core;
 using Bonobo.Git.Server.Models;
 
 namespace Bonobo.Git.Server.Data
