6a2d5bf8dcb2bb420d181d1a12cb311ca312e58b
Stan <s.shillis@gmail.com>
2014-08-06 22:15:34 -0400

    Basic receive-pack handling wrapper classes added. Not yet fully functional.

---
+++ Bonobo.Git.Server/Git/GitService/ReceivePackHook/ParsedPack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bonobo.Git.Server.Git.GitService.ReceivePackHook
+{
+    public class ParsedPack
+    {
+        public ParsedPack (string packId, string repositoryName, IEnumerable<ParsedPackRefHeader> headers, string pushedByUser, DateTime timestamp)
+	    {
+            this.PackId = packId;
+            this.Headers = headers;
+            this.PushedByUser = pushedByUser;
+            this.Timestamp = timestamp;
+            this.RepositoryName = repositoryName;
+	    }
+
+        public string PackId { get; private set; }
+
+        public IEnumerable<ParsedPackRefHeader> Headers { get; private set; }
+
+        public string PushedByUser { get; private set; }
+
+        public DateTime Timestamp { get; private set; }
+
+        public string RepositoryName { get; private set; }
+    }
+}
---
+++ Bonobo.Git.Server/Git/GitService/ReceivePackHook/ReceivePackCommits.cs
@@ -0,0 +1,21 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bonobo.Git.Server.Git.GitService.ReceivePackHook
+{
+    public class ReceivePackCommits
+    {
+        public ReceivePackCommits(string refName, ICommitLog commits)
+        {
+            this.RefName = refName;
+            this.Commits = commits;
+        }
+
+        public string RefName { get; private set; }
+
+        public ICommitLog Commits { get; private set; }
+    }
+}
