64ac15e244ac9cbd72c287b180a8d7842a06e0d1
stanshillis <s.shillis@gmail.com>
2014-08-11 20:42:42 -0400

    Moved time configuration into constructor parameter

--- Bonobo.Git.Server/Git/GitService/ReceivePackHook/Hooks/DurableReceivePackHook.cs
+++ Bonobo.Git.Server/Git/GitService/ReceivePackHook/Hooks/DurableReceivePackHook.cs
@@ -7,20 +7,31 @@ using System.Web;
 namespace Bonobo.Git.Server.Git.GitService.ReceivePackHook.Hooks
 {
     /// <summary>
+    /// Perhaps there's a better way to handle this in Unity but i haven't found it
+    /// </summary>
+    public class FailedPackWaitTimeBeforeExecution
+    {
+        public FailedPackWaitTimeBeforeExecution(TimeSpan timeSpan)
+        {
+            this.Value = timeSpan;
+        }
+        public TimeSpan Value { get; private set; }
+    }
+
+    /// <summary>
     /// Provides at least once execution guarantee to PostPackReceive hook method
     /// </summary>
     public class DurableReceivePackHook : IHookReceivePack
     {
-        private readonly int failedPackWaitTimeBeforeExecutionInSec;
+        private readonly TimeSpan failedPackWaitTimeBeforeExecution;
         private readonly IHookReceivePack next;
         private readonly IReceivePackRepository receivePackRepo;
 
-        // TODO: change failedPackWaitTimeBeforeExecutionInSec to be an injection param
-        public DurableReceivePackHook(IHookReceivePack next, IReceivePackRepository receivePackRepo) // int failedPackWaitTimeBeforeExecutionInSec)
+        public DurableReceivePackHook(IHookReceivePack next, IReceivePackRepository receivePackRepo, FailedPackWaitTimeBeforeExecution failedPackWaitTimeBeforeExecution)
         {
             this.next = next;
             this.receivePackRepo = receivePackRepo;
-            this.failedPackWaitTimeBeforeExecutionInSec = 5 * 60; //failedPackWaitTimeBeforeExecutionInSec;
+            this.failedPackWaitTimeBeforeExecution = failedPackWaitTimeBeforeExecution.Value;
         }
 
         public void PrePackReceive(ParsedReceivePack receivePack)
@@ -36,7 +47,7 @@ namespace Bonobo.Git.Server.Git.GitService.ReceivePackHook.Hooks
                 // execute PostPackReceive right away only for the current pack
                 // or if the pack has been sitting in database for X amount of time
                 if (pack.PackId == receivePack.PackId
-                    || (DateTime.Now - pack.Timestamp).TotalSeconds >= failedPackWaitTimeBeforeExecutionInSec)
+                    || (DateTime.Now - pack.Timestamp) >= failedPackWaitTimeBeforeExecution)
                 {
                     next.PostPackReceive(pack);
                     receivePackRepo.Delete(pack.PackId);
