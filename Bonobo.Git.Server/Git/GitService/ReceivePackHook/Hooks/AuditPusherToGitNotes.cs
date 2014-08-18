9f83466dfe5397478ee59166d4dd0d09c820fa1a
stanshillis <s.shillis@gmail.com>
2014-08-18 13:33:37 -0400

    stopped using default notes user signature because it doesn't work in all cases

--- Bonobo.Git.Server/Git/GitService/ReceivePackHook/Hooks/AuditPusherToGitNotes.cs
+++ Bonobo.Git.Server/Git/GitService/ReceivePackHook/Hooks/AuditPusherToGitNotes.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using LibGit2Sharp;
+using Bonobo.Git.Server.Security;
 
 namespace Bonobo.Git.Server.Git.GitService.ReceivePackHook.Hooks
 {
@@ -13,12 +14,14 @@ namespace Bonobo.Git.Server.Git.GitService.ReceivePackHook.Hooks
         private IGitRepositoryLocator repoLocator;
         private IHookReceivePack next;
         private Bonobo.Git.Server.Data.IRepositoryRepository repoConfig;
+        private readonly IMembershipService userRepo;
 
-        public AuditPusherToGitNotes(IHookReceivePack next, IGitRepositoryLocator repoLocator, Bonobo.Git.Server.Data.IRepositoryRepository repoConfig)
+        public AuditPusherToGitNotes(IHookReceivePack next, IGitRepositoryLocator repoLocator, Bonobo.Git.Server.Data.IRepositoryRepository repoConfig, IMembershipService userRepo)
         {
             this.next = next;
             this.repoLocator = repoLocator;
             this.repoConfig = repoConfig;
+            this.userRepo = userRepo;
         }
 
         public void PostPackReceive(ParsedReceivePack receivePack)
@@ -27,9 +30,15 @@ namespace Bonobo.Git.Server.Git.GitService.ReceivePackHook.Hooks
             if (repo.AuditPushUser == true)
             {
                 var user = receivePack.PushedByUser;
+                var email = "";
                 if (string.IsNullOrEmpty(user))
                 {
                     user = EMPTY_USER;
+                } else {
+                    var userData = userRepo.GetUser(user);
+                    if(userData != null) {
+                        email = userData.Email;
+                    }
                 }
 
                 var gitRepo = new Repository(repoLocator.GetRepositoryDirectoryPath(receivePack.RepositoryName).FullName);
@@ -47,6 +56,8 @@ namespace Bonobo.Git.Server.Git.GitService.ReceivePackHook.Hooks
                         gitRepo.Notes.Add(
                             commit.Id,
                             user,
+                            new Signature(user, email, DateTimeOffset.Now),
+                            new Signature(user, email, DateTimeOffset.Now),
                             "pusher");
                     }
                 }
