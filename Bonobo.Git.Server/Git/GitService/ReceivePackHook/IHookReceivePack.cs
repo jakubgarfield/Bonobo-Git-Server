0731948ef56a818f928b381a19a400eb5a70bef0
stanshillis <s.shillis@gmail.com>
2014-08-28 22:00:06 -0400

    Basic receive-pack handling wrapper classes added. Not yet fully functional.

---
+++ Bonobo.Git.Server/Git/GitService/ReceivePackHook/IHookReceivePack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bonobo.Git.Server.Git.GitService.ReceivePackHook
+{
+    /// <summary>
+    /// Implement this interface to receive notifications when a pack is recieved
+    /// and perform any relevant post-processing operations.
+    /// </summary>
+    public interface IHookReceivePack
+    {
+        void PackReceived(string packId, string repositoryName, DateTime timestamp, IEnumerable<ReceivePackCommits> commitData, string pushedByUser);
+    }
+}
