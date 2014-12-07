c9b0665d1e25925c68aa8f8a5c6ba28f1e91ad15
Kyle Farnung <kfarnung@outlook.com>
2014-12-07 02:41:32 -0800

    Fixing issue #171 - plus some cleanup tasks
    Fixing System.ArgumentOutOfRangeException: Non-negative number required.
    
    The biggest issue was fixed by switching to an unbuffered stream when
    receiving a pack.  We also set the flag to bypass the httpRuntime
    maxRequestLength, theoretically allowing upload of a pack of any size.
    
    Also fixed various other issues related to large repos.  Git seems to
    have a bug that although it's running a 32-bit executable, it detects a
    64-bit system and uses the 64-bit config values.  This causes the
    process to run out of memory when compressing large sets of objects.
    This was resolved by adding a git config file in App_Data and updating
    the code to provide the HOME environment variable to the Git process.
    The values in the config file are reasonable defaults that allow cloning
    of a 22GB repo without any issues.
    
    Also fixed the maxAllowedContentLength to be 100MB to match the
    maxRequestLength.  This doesn't seem to be used on IIS8, but the
    previous 4MB value was way too small.
    
    Various cleanup tasks in touched files:  putting using inside namespace
    (http://stackoverflow.com/questions/125319/should-using-statements-be-inside-or-outside-the-namespace),
    making lines more uniform.

--- Bonobo.Git.Server/Controllers/GitController.cs
+++ Bonobo.Git.Server/Controllers/GitController.cs
@@ -1,17 +1,16 @@
-﻿using System.Configuration;
-using Bonobo.Git.Server.Configuration;
-using Bonobo.Git.Server.Security;
-using Microsoft.Practices.Unity;
-using System;
-using System.IO;
-using System.Web.Mvc;
-using Bonobo.Git.Server.Git.GitService;
-using System.Web;
-using Ionic.Zlib;
-using Bonobo.Git.Server.Git;
-
-namespace Bonobo.Git.Server.Controllers
+﻿namespace Bonobo.Git.Server.Controllers
 {
+    using System;
+    using System.IO;
+    using System.Web.Mvc;
+    using Bonobo.Git.Server.Configuration;
+    using Bonobo.Git.Server.Git;
+    using Bonobo.Git.Server.Git.GitService;
+    using Bonobo.Git.Server.Security;
+    using Ionic.Zlib;
+    using LibGit2Sharp;
+    using Microsoft.Practices.Unity;
+
     [GitAuthorize]
     public class GitController : Controller
     {
@@ -23,57 +22,62 @@ namespace Bonobo.Git.Server.Controllers
 
         public ActionResult SecureGetInfoRefs(String project, String service)
         {
-            if (RepositoryPermissionService.HasPermission(User.Identity.Name, project)
-                || (RepositoryPermissionService.AllowsAnonymous(project)
-                    && (String.Equals("git-upload-pack", service, StringComparison.InvariantCultureIgnoreCase)
+            if (this.RepositoryPermissionService.HasPermission(this.User.Identity.Name, project)
+                || (this.RepositoryPermissionService.AllowsAnonymous(project)
+                    && (String.Equals("git-upload-pack", service, StringComparison.OrdinalIgnoreCase)
                         || UserConfiguration.Current.AllowAnonymousPush)))
             {
-                return GetInfoRefs(project, service);
+                return this.GetInfoRefs(project, service);
             }
             else
             {
-                return UnauthorizedResult();
+                return this.UnauthorizedResult();
             }
         }
 
         [HttpPost]
         public ActionResult SecureUploadPack(String project)
         {
-            if (RepositoryPermissionService.HasPermission(User.Identity.Name, project)
-                || RepositoryPermissionService.AllowsAnonymous(project))
+            if (this.RepositoryPermissionService.HasPermission(this.User.Identity.Name, project)
+                || this.RepositoryPermissionService.AllowsAnonymous(project))
             {
-                return ExecuteUploadPack(project);
+                return this.ExecuteUploadPack(project);
             }
             else
             {
-                return UnauthorizedResult();
+                return this.UnauthorizedResult();
             }
         }
 
         [HttpPost]
         public ActionResult SecureReceivePack(String project)
         {
-            if (RepositoryPermissionService.HasPermission(User.Identity.Name, project)
-                || (RepositoryPermissionService.AllowsAnonymous(project) && UserConfiguration.Current.AllowAnonymousPush))
+            if (this.RepositoryPermissionService.HasPermission(this.User.Identity.Name, project)
+                || (this.RepositoryPermissionService.AllowsAnonymous(project) && UserConfiguration.Current.AllowAnonymousPush))
             {
-                return ExecuteReceivePack(project);
+                return this.ExecuteReceivePack(project);
             }
             else
             {
-                return UnauthorizedResult();
+                return this.UnauthorizedResult();
             }
         }
-
 
         private ActionResult ExecuteReceivePack(string project)
         {
             var directory = GetDirectoryInfo(project);
-            if (LibGit2Sharp.Repository.IsValid(directory.FullName))
+            if (Repository.IsValid(directory.FullName))
             {
-                return new GitCmdResult("application/x-git-receive-pack-result", (outStream) =>
-                {
-                    GitService.ExecuteGitReceivePack(Guid.NewGuid().ToString("N"), project, GetInputStream(), outStream);
-                });
+                return new GitCmdResult(
+                    "application/x-git-receive-pack-result",
+                    (outStream) =>
+                    {
+                        this.GitService.ExecuteGitReceivePack(
+                            Guid.NewGuid().ToString("N"),
+                            project,
+                            GetInputStream(disableBuffer: true),
+                            outStream);
+                    });
             }
             else
             {
@@ -84,12 +88,18 @@ namespace Bonobo.Git.Server.Controllers
         private ActionResult ExecuteUploadPack(string project)
         {
             var directory = GetDirectoryInfo(project);
-            if (LibGit2Sharp.Repository.IsValid(directory.FullName))
+            if (Repository.IsValid(directory.FullName))
             {
-                return new GitCmdResult("application/x-git-upload-pack-result", (outStream) =>
-                {
-                    GitService.ExecuteGitUploadPack(Guid.NewGuid().ToString("N"), project, GetInputStream(), outStream);
-                });
+                return new GitCmdResult(
+                    "application/x-git-upload-pack-result",
+                    (outStream) =>
+                    {
+                        this.GitService.ExecuteGitUploadPack(
+                            Guid.NewGuid().ToString("N"),
+                            project,
+                            this.GetInputStream(),
+                            outStream);
+                    });
             }
             else
             {
@@ -100,26 +110,28 @@ namespace Bonobo.Git.Server.Controllers
         private ActionResult GetInfoRefs(String project, String service)
         {
             var directory = GetDirectoryInfo(project);
-            if (LibGit2Sharp.Repository.IsValid(directory.FullName))
+            if (Repository.IsValid(directory.FullName))
             {
-                Response.StatusCode = 200;
+                this.Response.StatusCode = 200;
 
                 string contentType = String.Format("application/x-{0}-advertisement", service);
                 string serviceName = service.Substring(4);
                 string advertiseRefsContent = FormatMessage(String.Format("# service={0}\n", service)) + FlushMessage();
 
-                return new GitCmdResult(contentType, (outStream) =>
-                {
-                    GitService.ExecuteServiceByName(
-                        Guid.NewGuid().ToString("N"),
-                        project, 
-                        serviceName, 
-                        new ExecutionOptions() { AdvertiseRefs = true },
-                        GetInputStream(),
-                        outStream
-                    );
-                }, 
-                advertiseRefsContent);
+                return new GitCmdResult(
+                    contentType,
+                    (outStream) =>
+                    {
+                        this.GitService.ExecuteServiceByName(
+                            Guid.NewGuid().ToString("N"),
+                            project, 
+                            serviceName, 
+                            new ExecutionOptions() { AdvertiseRefs = true },
+                            this.GetInputStream(),
+                            outStream
+                        );
+                    }, 
+                    advertiseRefsContent);
             }
             else
             {
@@ -129,8 +141,9 @@ namespace Bonobo.Git.Server.Controllers
 
         private ActionResult UnauthorizedResult()
         {
-            Response.Clear();
-            Response.AddHeader("WWW-Authenticate", "Basic realm=\"Secure Area\"");
+            this.Response.Clear();
+            this.Response.AddHeader("WWW-Authenticate", "Basic realm=\"Secure Area\"");
+            
             return new HttpStatusCodeResult(401);
         }
 
@@ -149,9 +162,17 @@ namespace Bonobo.Git.Server.Controllers
             return new DirectoryInfo(Path.Combine(UserConfiguration.Current.Repositories, project));
         }
 
-        private Stream GetInputStream()
+        private Stream GetInputStream(bool disableBuffer = false)
         {
-            return this.Request.Headers["Content-Encoding"] == "gzip" ? new GZipStream(this.Request.InputStream, CompressionMode.Decompress) : this.Request.InputStream;
+            // For really large uploads we need to get a bufferless input stream and disable the max
+            // request length.
+            Stream requestStream = disableBuffer ?
+                this.Request.GetBufferlessInputStream(disableMaxRequestLength: true) :
+                this.Request.GetBufferedInputStream();
+
+            return this.Request.Headers["Content-Encoding"] == "gzip" ?
+                new GZipStream(requestStream, CompressionMode.Decompress) :
+                requestStream;
         }
     }
 }
--- Bonobo.Git.Server/Git/GitService/GitServiceExecutor.cs
+++ Bonobo.Git.Server/Git/GitService/GitServiceExecutor.cs
@@ -1,40 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Web;
-
-namespace Bonobo.Git.Server.Git.GitService
+﻿namespace Bonobo.Git.Server.Git.GitService
 {
+    using System.Diagnostics;
+    using System.IO;
+
     // perhaps can be done cleaner than this, but i can't figure out how to
     // to do registration in Unity to handle 2 string constructor parameters
     public class GitServiceExecutorParams
     {
         public string GitPath { get; set; }
+        
+        public string GitHomePath { get; set; }
+        
         public string RepositoriesDirPath { get; set; }
     }
-
 
     public class GitServiceExecutor : IGitService
     {
         private readonly string gitPath;
+        private readonly string gitHomePath;
         private readonly string repositoriesDirPath;
         private readonly IGitRepositoryLocator repoLocator;
 
         public GitServiceExecutor(GitServiceExecutorParams parameters, IGitRepositoryLocator repoLocator)
         {
             this.gitPath = parameters.GitPath;
+            this.gitHomePath = parameters.GitHomePath;
             this.repositoriesDirPath = parameters.RepositoriesDirPath;
             this.repoLocator = repoLocator;
         }
 
-        public void ExecuteServiceByName(string correlationId, string repositoryName, string serviceName, ExecutionOptions options, System.IO.Stream inStream, System.IO.Stream outStream)
+        public void ExecuteServiceByName(
+            string correlationId,
+            string repositoryName,
+            string serviceName,
+            ExecutionOptions options,
+            Stream inStream,
+            Stream outStream)
         {
             var args = serviceName + " --stateless-rpc";
             args += options.ToCommandLineArgs();
-            args += " \"" + repoLocator.GetRepositoryDirectoryPath(repositoryName).FullName + "\"";
+            args += " \"" + this.repoLocator.GetRepositoryDirectoryPath(repositoryName).FullName + "\"";
 
-            var info = new System.Diagnostics.ProcessStartInfo(gitPath, args)
+            var info = new ProcessStartInfo(this.gitPath, args)
             {
                 CreateNoWindow = true,
                 RedirectStandardError = true,
@@ -44,19 +51,15 @@ namespace Bonobo.Git.Server.Git.GitService
                 WorkingDirectory = Path.GetDirectoryName(this.repositoriesDirPath),
             };
 
-            using (var process = System.Diagnostics.Process.Start(info))
+            // Set the HOME environment so that Git can find the config file.
+            info.EnvironmentVariables.Add("HOME", this.gitHomePath);
+
+            using (var process = Process.Start(info))
             {
                 inStream.CopyTo(process.StandardInput.BaseStream);
                 process.StandardInput.Write('\0');
 
-                var buffer = new byte[16 * 1024];
-                int read;
-                while ((read = process.StandardOutput.BaseStream.Read(buffer, 0, buffer.Length)) > 0)
-                {
-                    outStream.Write(buffer, 0, read);
-                    outStream.Flush();
-                }
-
+                process.StandardOutput.BaseStream.CopyTo(outStream);
                 process.WaitForExit();
             }
         }
