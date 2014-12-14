b5cceb0e68ea36db3fab8567e76a3ba92f533ad7
Kyle Engibous <kyle.engibous@gmail.com>
2014-12-14 13:10:01 -0600

    Fix to allow email addresses as users names, Issue #163 #158
    Added a regular expression to validate if the username is an email
    address or not.  If the username is an email address then the username
    will not be encoded/decoded.

--- Bonobo.Git.Server/UsernameUrl.cs
+++ Bonobo.Git.Server/UsernameUrl.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace Bonobo.Git.Server
 {
     public class UsernameUrl
     {
+        //to allow support for email addresses as user names, only encode/decode user name if it is not an email address
+        private static Regex _isEmailRegEx = new Regex(
+            @"^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$",
+            RegexOptions.Compiled);
+
         public static string Encode(string username)
         {
             var nameParts = username.Split('\\');
-            if (nameParts.Count() == 2)
+            if ( nameParts.Count() == 2  && !_isEmailRegEx.IsMatch(username) )
             {
                 return nameParts[1] + "@" + nameParts[0];
             }
@@ -21,7 +27,7 @@ namespace Bonobo.Git.Server
         public static string Decode(string username)
         {
             var nameParts = username.Split('@');
-            if (nameParts.Count() == 2)
+            if ( nameParts.Count() == 2 && !_isEmailRegEx.IsMatch(username) )
             {
                 return nameParts[1] + "\\" + nameParts[0];
             }
