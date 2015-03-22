d9871eb69693c121212280a568ea921aa4695e34
SanSYS <i.sansys@gmail.com>
2015-03-22 11:59:06 +0300

    NEW: minimize/maximize repository groups on client side
    toggling group state saved in localStorage

---
+++ Bonobo.Git.Server/Scripts/RepositoryGroupToggle.js
@@ -0,0 +1,35 @@
+﻿;(function ($) {
+    'use strict';
+
+    /* TODO: when jQuery will be updated - replace subscription by:
+     *             $('.group').on('click', '.toggle', function () {
+     */
+    $('.group .toggle').click('click', function () {
+        var self = $(this),
+            repositories = self.closest('.pure-u-1').next('.pure-u-1'),
+            group = self.data('group');
+
+        repositories.toggle();
+
+        self.toggleClass('on');
+
+        if (window.localStorage) {
+            if (self.hasClass('on')){
+                window.localStorage.setItem(group, "false");
+            } else{
+                window.localStorage.removeItem(group);
+            }
+        }
+    });
+
+    if (window.localStorage) {
+        $('.group .toggle').each(function () {
+            var self = $(this),
+                group = self.data('group');
+
+            if (window.localStorage.getItem(group) === "false") {
+                self.click();
+            }
+        });
+    }
+})(jQuery);
