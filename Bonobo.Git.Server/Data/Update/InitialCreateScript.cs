74d0556885b79bdada40a70544b289986d1151b4
Guilherme Bufolo <guibufolo@gmail.com>
2016-01-23 14:35:40 +0530

    Update DB creation and update scripts to have Id fields.

--- Bonobo.Git.Server/Data/Update/InitialCreateScript.cs
+++ Bonobo.Git.Server/Data/Update/InitialCreateScript.cs
@@ -14,71 +14,71 @@ namespace Bonobo.Git.Server.Data.Update
                 return @"
 
                     CREATE TABLE IF NOT EXISTS [Repository] (
-                        [Name] VarChar(255) Not Null,
+                        [Id] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                        [Name] VarChar(255) Not Null UNIQUE,
                         [Description] VarChar(255) Null,
-                        [Anonymous] Bit Not Null,
-                        Constraint [PK_Repository] Primary Key ([Name])
+                        [Anonymous] Bit Not Null
                     );
 
                     CREATE TABLE IF NOT EXISTS [Role] (
-                        [Name] VarChar(255) Not Null,
-                        [Description] VarChar(255) Null,
-                        Constraint [PK_Role] Primary Key ([Name])
+                        [Id] INTEGER PRIMARY KEY AUTOINCREMENT,
+                        [Name] VarChar(255) Not Null UNIQUE,
+                        [Description] VarChar(255) Null
                     );
 
                     CREATE TABLE IF NOT EXISTS [Team] (
-                        [Name] VarChar(255) Not Null,
-                        [Description] VarChar(255) Null,
-                        Constraint [PK_Team] Primary Key ([Name])
+                        [Id] INTEGER PRIMARY KEY AUTOINCREMENT,
+                        [Name] VarChar(255) Not Null UNIQUE,
+                        [Description] VarChar(255) Null
                     );
 
                     CREATE TABLE IF NOT EXISTS [User] (
+                        [Id] INTEGER PRIMARY KEY AUTOINCREMENT,
                         [Name] VarChar(255) Not Null,
                         [Surname] VarChar(255) Not Null,
-                        [Username] VarChar(255) Not Null,
+                        [Username] VarChar(255) Not Null UNIQUE,
                         [Password] VarChar(255) Not Null,
-                        [Email] VarChar(255) Not Null,
-                        Constraint [PK_User] Primary Key ([Username])
+                        [Email] VarChar(255) Not Null
                     );
 
                     CREATE TABLE IF NOT EXISTS [TeamRepository_Permission] (
-                        [Team_Name] VarChar(255) Not Null,
-                        [Repository_Name] VarChar(255) Not Null,
-                        Constraint [UNQ_TeamRepository_Permission_1] Unique ([Team_Name], [Repository_Name]),
-                        Foreign Key ([Team_Name]) References [Team]([Name]),
-                        Foreign Key ([Repository_Name]) References [Repository]([Name])
+                        [Team_Id] Integer Not Null,
+                        [Repository_Id] Integer Not Null,
+                        Constraint [UNQ_TeamRepository_Permission_1] Unique ([Team_Id], [Repository_Id]),
+                        Foreign Key ([Team_Id]) References [Team]([Id]),
+                        Foreign Key ([Repository_Id]) References [Repository]([Id])
                     );
 
                     CREATE TABLE IF NOT EXISTS [UserRepository_Administrator] (
-                        [User_Username] VarChar(255) Not Null,
-                        [Repository_Name] VarChar(255) Not Null,
-                        Constraint [UNQ_UserRepository_Administrator_1] Unique ([User_Username], [Repository_Name]),
-                        Foreign Key ([User_Username]) References [User]([Username]),
-                        Foreign Key ([Repository_Name]) References [Repository]([Name])
+                        [User_Id] INTEGER Not Null,
+                        [Repository_Id] INTEGER Not Null,
+                        Constraint [UNQ_UserRepository_Administrator_1] Unique ([User_Id], [Repository_Id]),
+                        Foreign Key ([User_Id]) References [User]([Id]),
+                        Foreign Key ([Repository_Id]) References [Repository]([Id])
                     );
 
                     CREATE TABLE IF NOT EXISTS [UserRepository_Permission] (
-                        [User_Username] VarChar(255) Not Null,
-                        [Repository_Name] VarChar(255) Not Null,
-                        Constraint [UNQ_UserRepository_Permission_1] Unique ([User_Username], [Repository_Name]),
-                        Foreign Key ([User_Username]) References [User]([Username]),
-                        Foreign Key ([Repository_Name]) References [Repository]([Name])
+                        [User_Id] INTEGER Not Null,
+                        [Repository_Id] INTEGER Not Null,
+                        Constraint [UNQ_UserRepository_Permission_1] Unique ([User_Id], [Repository_Id]),
+                        Foreign Key ([User_Id]) References [User]([Id]),
+                        Foreign Key ([Repository_Id]) References [Repository]([Id])
                     );
 
                     CREATE TABLE IF NOT EXISTS [UserRole_InRole] (
-                        [User_Username] VarChar(255) Not Null,
-                        [Role_Name] VarChar(255) Not Null,
-                        Constraint [UNQ_UserRole_InRole_1] Unique ([User_Username], [Role_Name]),
-                        Foreign Key ([User_Username]) References [User]([Username]),
-                        Foreign Key ([Role_Name]) References [Role]([Name])
+                        [User_Id] INTEGER Not Null,
+                        [Role_Id] INTEGER Not Null,
+                        Constraint [UNQ_UserRole_InRole_1] Unique ([User_Id], [Role_Id]),
+                        Foreign Key ([User_Id]) References [User]([Id]),
+                        Foreign Key ([Role_Id]) References [Role]([Id])
                     );
 
                     CREATE TABLE IF NOT EXISTS [UserTeam_Member] (
-                        [User_Username] VarChar(255) Not Null,
-                        [Team_Name] VarChar(255) Not Null,
-                        Constraint [UNQ_UserTeam_Member_1] Unique ([User_Username], [Team_Name]),
-                        Foreign Key ([User_Username]) References [User]([Username]),
-                        Foreign Key ([Team_Name]) References [Team]([Name])
+                        [User_Id] INTEGER Not Null,
+                        [Team_Id] INTEGER Not Null,
+                        Constraint [UNQ_UserTeam_Member_1] Unique ([User_Id], [Team_Id]),
+                        Foreign Key ([User_Id]) References [User]([Id]),
+                        Foreign Key ([Team_Id]) References [Team]([Id])
                     );
 
                     ";
--- Bonobo.Git.Server/Data/Update/InsertDefaultData.cs
+++ Bonobo.Git.Server/Data/Update/InsertDefaultData.cs
@@ -15,7 +15,7 @@ namespace Bonobo.Git.Server.Data.Update
 
                     INSERT INTO [Role] ([Name], [Description]) VALUES ('Administrator','System administrator');
                     INSERT INTO [User] ([Name], [Surname], [Username], [Password], [Email]) VALUES ('admin', '', 'admin', '0CC52C6751CC92916C138D8D714F003486BF8516933815DFC11D6C3E36894BFA044F97651E1F3EEBA26CDA928FB32DE0869F6ACFB787D5A33DACBA76D34473A3', '');
-                    INSERT INTO [UserRole_InRole] ([User_Username], [Role_Name]) VALUES ('admin', 'Administrator');
+                    INSERT INTO [UserRole_InRole] ([User_Id], [Role_Id]) VALUES (1, 1);
 
                     ";
             }
