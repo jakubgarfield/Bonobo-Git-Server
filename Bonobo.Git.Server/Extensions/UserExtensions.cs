f3032b32204a0d26c48b6746ca8fcbb0e6002af0
Will Dean <will.dean@indcomp.co.uk>
2016-02-17 18:10:30 +0000

    Avoid a NullRef exception being endlessly thrown in Claims code
    Logging exceptions

--- Bonobo.Git.Server/Extensions/UserExtensions.cs
+++ Bonobo.Git.Server/Extensions/UserExtensions.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Security;
 using System.Security.Claims;
 using System.Security.Principal;
-using System.Web;
 
 namespace Bonobo.Git.Server
 {
@@ -12,21 +10,23 @@ namespace Bonobo.Git.Server
     {
         public static string GetClaim(this IPrincipal user, string claimName)
         {
-            string result = null;
-
             try
             {
                 ClaimsIdentity claimsIdentity = GetClaimsIdentity(user);
                 if (claimsIdentity != null)
                 {
-                    result = claimsIdentity.FindFirst(claimName).Value;
+                    var claim = claimsIdentity.FindFirst(claimName);
+                    if (claim != null)
+                    {
+                        return claim.Value;
+                    }
                 }
             }
-            catch
+            catch(Exception ex)
             {
+                Trace.TraceError("GetClaim Exception " + ex);
             }
-
-            return result;
+            return null;
         }
 
         public static string Id(this IPrincipal user)
@@ -57,8 +57,9 @@ namespace Bonobo.Git.Server
                     result = claimsIdentity.FindAll(ClaimTypes.Role).Select(x => x.Value).ToArray();
                 }
             }
-            catch
+            catch(Exception ex)
             {
+                Trace.TraceError("GetClaim Exception " + ex);
             }
 
             return result;
@@ -71,7 +72,7 @@ namespace Bonobo.Git.Server
             ClaimsPrincipal claimsPrincipal = user as ClaimsPrincipal;
             if (claimsPrincipal != null)
             {
-                result = claimsPrincipal.Identities.FirstOrDefault(x => x is ClaimsIdentity);
+                result = claimsPrincipal.Identities.FirstOrDefault(x => x != null);
             }
 
             return result;
