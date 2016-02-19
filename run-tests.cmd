16ba09a1860023d2535f72a9a600f3f563a626b2
Guilherme Bufolo <guibufolo@gmail.com>
2016-02-19 17:37:10 +0530

    Add and fix IIS Settings to be able to run Integration tests.

--- run-tests.cmd
+++ run-tests.cmd
@@ -4,13 +4,9 @@ set config=Debug
 
 rem END OF CONFIGURATIONS
 
-set curpath=%CD%
-
 call setup-env.cmd
 
 msbuild Bonobo.Git.Server.sln /m /property:Configuration=%config%
 if exist Bonobo.Git.Server.Test/bin/%config%/Bonobo.Git.Server.Test.dll (
-	mstest /testcontainer:Bonobo.Git.Server.Test/bin/%config%/Bonobo.Git.Server.Test.dll
+	vstest.console Bonobo.Git.Server.Test/bin/%config%/Bonobo.Git.Server.Test.dll
 )
-
-cd %curpath%
