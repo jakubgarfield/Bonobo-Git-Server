3ad24c0953e6bfccb342efe1656055aa8a9c52c6
Guilherme Bufolo <guibufolo@gmail.com>
2016-02-19 17:23:46 +0530

    Added script to fetch portable git from github.
    This unfortunately requires 7z to be in the PATH.
    Added proxy support for nuget-packages and get-git scripts.

--- setup-env.cmd
+++ setup-env.cmd
@@ -1,3 +1,5 @@
 
+@rem You can pass /p:Proxy=http://127.0.0.1:8080 to use a proxy for download. This works also on get-git.msbuild
 msbuild nuget-packages.msbuild
-
+@rem You can pass /p:GitVersion=2.5.1 to get a certain git version.
+msbuild get-git.msbuild
