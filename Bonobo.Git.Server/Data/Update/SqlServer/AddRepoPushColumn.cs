3ebe0f3ed77e6b47509dfe7302e8f45a6109f795
Guilherme Bufolo <guibufolo@gmail.com>
2016-03-04 08:30:25 +0530

    Added update scripts to add needed column.
    SqlServer update scripts untested.

---
+++ Bonobo.Git.Server/Data/Update/SqlServer/AddRepoPushColumn.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bonobo.Git.Server.Data.Update.SqlServer
+{
+    public class AddRepoPushColumn : IUpdateScript
+    {
+        public string Command
+        {
+            get
+            {
+                return "ALTER TABLE Repository ADD [AllowAnonymousPush] TinyInt NOT NULL CONSTRAINT Def_Var_AAP Default 3";
+            }
+        }
+
+        public string Precondition
+        {
+            get
+            {
+                return @"
+            IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'Repository' AND  COLUMN_NAME = 'AllowAnonymousPush')
+                SELECT 0
+            ELSE
+                SELECT 1";
+            }
+        }
+
+        public void CodeAction(BonoboGitServerContext context) { }
+
+    }
+}
---
+++ Bonobo.Git.Server/Data/Update/Sqlite/AddRepoPushColumn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bonobo.Git.Server.Data.Update.Sqlite
+{
+    public class AddRepoPushColumn : IUpdateScript
+    {
+        public string Command
+        {
+            get
+            {
+                return "ALTER TABLE Repository ADD COLUMN [AllowAnonymousPush] INTEGER DEFAULT(3)";
+            }
+        }
+
+        public string Precondition
+        {
+            get
+            {
+                return "SELECT Count([AllowAnonymousPush]) = -1 FROM Repository";
+            }
+        }
+
+        public void CodeAction(BonoboGitServerContext context) { }
+
+    }
+}
