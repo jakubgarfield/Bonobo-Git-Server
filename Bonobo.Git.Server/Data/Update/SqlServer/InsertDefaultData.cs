4c77dfa3391f297bddbf0e43a008528366f8cc5e
Will Dean <will.dean@indcomp.co.uk>
2016-03-21 22:26:20 +0000

    Fixed sqlserver admin-create script

--- Bonobo.Git.Server/Data/Update/SqlServer/InsertDefaultData.cs
+++ Bonobo.Git.Server/Data/Update/SqlServer/InsertDefaultData.cs
@@ -6,13 +6,17 @@ namespace Bonobo.Git.Server.Data.Update.SqlServer
     {
         public string Command
         {
-            get { return @"
+            get
+            {
+                Guid roleId = new Guid("a3139d2b-5a59-427f-bb2d-af251dce00e4");
+                Guid UserId = new Guid("3eb9995e-99e3-425a-b978-1409bdd61fb6");
+                return @"
 
-                    INSERT INTO [Role] ([Name], [Description]) VALUES ('Administrator','System administrator');
-                    INSERT INTO [User] ([Name], [Surname], [Username], [Password], [Email]) VALUES ('admin', '', 'admin', '0CC52C6751CC92916C138D8D714F003486BF8516933815DFC11D6C3E36894BFA044F97651E1F3EEBA26CDA928FB32DE0869F6ACFB787D5A33DACBA76D34473A3', '');
-                    INSERT INTO [UserRole_InRole] ([User_Username], [Role_Name]) VALUES ('admin', 'Administrator');
-
-                    "; }
+                    INSERT INTO [Role] ([Id], [Name], [Description]) VALUES ('" + roleId +@"','Administrator','System administrator');
+                    INSERT INTO [User] ([Id], [Name], [Surname], [Username], [Password], [PasswordSalt], [Email]) VALUES ('" +UserId +@"','admin', '', 'admin', '0CC52C6751CC92916C138D8D714F003486BF8516933815DFC11D6C3E36894BFA044F97651E1F3EEBA26CDA928FB32DE0869F6ACFB787D5A33DACBA76D34473A3', 'admin', '');
+                    INSERT INTO [UserRole_InRole] ([User_Id], [Role_Id]) VALUES ('" + UserId + "','" + roleId + @"');
+                    ";
+            }
         }
 
         public string Precondition
