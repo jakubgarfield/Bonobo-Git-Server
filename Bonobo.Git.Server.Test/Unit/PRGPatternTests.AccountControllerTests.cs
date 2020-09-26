2471030366cfa934ff77fe1e1acd68d88abbb078
Eugenio Miró <eugenio.miro@gmail.com>
2020-09-26 09:09:11 -0300

    reorder tests in source code
    (cherry picked from commit 20fbb81ba45087a1de760bdec1ab3391b9043434)

--- Bonobo.Git.Server.Test/Unit/PRGPatternTests.AccountControllerTests.cs
+++ Bonobo.Git.Server.Test/Unit/PRGPatternTests.AccountControllerTests.cs
@@ -24,6 +24,127 @@ namespace Bonobo.Git.Server.Test.Unit
             }
 
             [TestMethod]
+            public void Get_Edit__Cannot_Handle_Null_Parameters()
+            {
+                try
+                {
+                    // arrange
+                    SetHttpContextMockIntoSUT(Guid.Empty);
+                    // act
+                    var result = SutAs<AccountController>().Edit(null);
+                }
+                catch (NullReferenceException)
+                {
+                    return;
+                }
+                //assert
+                Assert.Fail();
+            }
+
+            [TestMethod]
+            public void Get_Edit_When_User_Is_Unknown_And_Resulting_Model_Is_Null__Stays_On_View()
+            {
+                // Arrange
+                Guid userid = Guid.NewGuid();
+                SetHttpContextMockIntoSUT(userid);
+                SetupMembershipServiceMockIntoSUT();
+                BindModelToController(userid);
+
+                // act
+                var result = SutAs<AccountController>().Edit(userid);
+
+                // assert
+                Assert.IsNotNull(result);
+                Assert.IsInstanceOfType(result, typeof(ViewResult));
+                var viewResult = result as ViewResult;
+                Assert.IsNotNull(viewResult);
+                Assert.IsNull(viewResult.Model);
+            }
+
+            [TestMethod]
+            public void Get_Edit_When_Non_Admin_User_Queries_Other_UserId__Gets_Redirected_To_Home_Unauthorized()
+            {
+                // Arrange
+                Guid userid = Guid.NewGuid();
+                SetHttpContextMockIntoSUT(userid);
+                BindModelToController(userid);
+
+                // act
+                var result = SutAs<AccountController>().Edit(Guid.NewGuid());
+
+                // assert
+                AssertRedirectToHomeUnauthorized(result);
+            }
+
+            [TestMethod]
+            public void Get_Edit_When_Admin_User_Queries_Other_User_Id_And_Resulting_Model_Is_Null__Stays_On_View()
+            {
+                // Arrange
+                Guid userid = Guid.NewGuid();
+                Guid otherId = Guid.NewGuid();
+                SetHttpContextMockIntoSUT(userid);
+                SetupMembershipServiceMockIntoSUT();
+                SetupUserAsAdmin();
+                BindModelToController(userid);
+
+                // act
+                var result = SutAs<AccountController>().Edit(otherId);
+                var viewResult = result as ViewResult;
+
+                // assert
+                Assert.IsNotNull(result);
+                Assert.IsInstanceOfType(result, typeof(ViewResult));
+                Assert.IsNotNull(viewResult);
+                Assert.IsNull(viewResult.Model);
+            }
+
+            [TestMethod]
+            public void Get_Edit_When_User_Is_Known_And_Resulting_Model_Is_Not_Null__Stays_On_View()
+            {
+                // Arrange
+                Guid userId = Guid.NewGuid();
+                SetHttpContextMockIntoSUT(userId);
+                SetupMembershipServiceMockIntoSUT();
+                BindModelToController(userId);
+                membershipServiceMock.Setup(m => m.GetUserModel(userId))
+                                     .Returns(new UserModel
+                                     {
+                                         Id = userId,
+                                         Username = "Username",
+                                         GivenName = "Given",
+                                         Surname = "Sur",
+                                         Email = "email"
+                                     });
+
+                string[] allRoles = new string[] { "role1", "role2" };
+                string[] selectedRoles = new string[] { "role1" };
+                SetupRolesProviderMockIntoSUT();
+                roleProviderMock.Setup(r => r.GetAllRoles())
+                                .Returns(allRoles);
+                roleProviderMock.Setup(r => r.GetRolesForUser(userId))
+                                .Returns(selectedRoles);
+
+                // act
+                var result = SutAs<AccountController>().Edit(userId);
+                var viewResult = result as ViewResult;
+                var userEditModel = viewResult?.Model as UserEditModel;
+
+                // assert
+                Assert.IsNotNull(result);
+                Assert.IsInstanceOfType(result, typeof(ViewResult));
+                Assert.IsNotNull(viewResult);
+                Assert.IsNotNull(viewResult.Model);
+                Assert.IsInstanceOfType(viewResult.Model, typeof(UserEditModel));
+                Assert.AreEqual(userEditModel.Id, userId);
+                Assert.AreEqual(userEditModel.Username, "Username");
+                Assert.AreEqual(userEditModel.Name, "Given");
+                Assert.AreEqual(userEditModel.Surname, "Sur");
+                Assert.AreEqual(userEditModel.Email, "email");
+                Assert.AreEqual(userEditModel.Roles, allRoles);
+                Assert.AreEqual(userEditModel.SelectedRoles, selectedRoles);
+            }
+
+            [TestMethod]
             public void Post_Edit_With_Unbound_Empty_Model_And_Environment_Unprepared__Throws_A_NullReferenceException()
             {
                 try
@@ -181,126 +302,6 @@ namespace Bonobo.Git.Server.Test.Unit
                 AssertSuccessfulResponse(result);
             }
 
-            [TestMethod]
-            public void Get_Edit__Cannot_Handle_Null_Parameters()
-            {
-                try
-                {
-                    // arrange
-                    SetHttpContextMockIntoSUT(Guid.Empty);
-                    // act
-                    var result = SutAs<AccountController>().Edit(null);
-                }
-                catch (NullReferenceException)
-                {
-                    return;
-                }
-                //assert
-                Assert.Fail();
-            }
-
-            [TestMethod]
-            public void Get_Edit_When_User_Is_Unknown_And_Resulting_Model_Is_Null__Stays_On_View()
-            {
-                // Arrange
-                Guid userid = Guid.NewGuid();
-                SetHttpContextMockIntoSUT(userid);
-                SetupMembershipServiceMockIntoSUT();
-                BindModelToController(userid);
-
-                // act
-                var result = SutAs<AccountController>().Edit(userid);
-
-                // assert
-                Assert.IsNotNull(result);
-                Assert.IsInstanceOfType(result, typeof(ViewResult));
-                var viewResult = result as ViewResult;
-                Assert.IsNotNull(viewResult);
-                Assert.IsNull(viewResult.Model);
-            }
-
-            [TestMethod]
-            public void Get_Edit_When_Non_Admin_User_Queries_Other_UserId__Gets_Redirected_To_Home_Unauthorized()
-            {
-                // Arrange
-                Guid userid = Guid.NewGuid();
-                SetHttpContextMockIntoSUT(userid);
-                BindModelToController(userid);
-
-                // act
-                var result = SutAs<AccountController>().Edit(Guid.NewGuid());
-
-                // assert
-                AssertRedirectToHomeUnauthorized(result);
-            }
-
-            [TestMethod]
-            public void Get_Edit_When_Admin_User_Queries_Other_User_Id_And_Resulting_Model_Is_Null__Stays_On_View()
-            {
-                // Arrange
-                Guid userid = Guid.NewGuid();
-                Guid otherId = Guid.NewGuid();
-                SetHttpContextMockIntoSUT(userid);
-                SetupMembershipServiceMockIntoSUT();
-                SetupUserAsAdmin();
-                BindModelToController(userid);
-
-                // act
-                var result = SutAs<AccountController>().Edit(otherId);
-                var viewResult = result as ViewResult;
-
-                // assert
-                Assert.IsNotNull(result);
-                Assert.IsInstanceOfType(result, typeof(ViewResult));
-                Assert.IsNotNull(viewResult);
-                Assert.IsNull(viewResult.Model);
-            }
-
-            [TestMethod]
-            public void Get_Edit_When_User_Is_Known_And_Resulting_Model_Is_Not_Null__Stays_On_View()
-            {
-                // Arrange
-                Guid userId = Guid.NewGuid();
-                SetHttpContextMockIntoSUT(userId);
-                SetupMembershipServiceMockIntoSUT();
-                BindModelToController(userId);
-                membershipServiceMock.Setup(m => m.GetUserModel(userId))
-                                     .Returns(new UserModel
-                                     {
-                                         Id = userId,
-                                         Username = "Username",
-                                         GivenName = "Given",
-                                         Surname = "Sur",
-                                         Email = "email"
-                                     });
-
-                string[] allRoles = new string[] { "role1", "role2" };
-                string[] selectedRoles = new string[] { "role1" };
-                SetupRolesProviderMockIntoSUT();
-                roleProviderMock.Setup(r => r.GetAllRoles())
-                                .Returns(allRoles);
-                roleProviderMock.Setup(r => r.GetRolesForUser(userId))
-                                .Returns(selectedRoles);
-
-                // act
-                var result = SutAs<AccountController>().Edit(userId);
-                var viewResult = result as ViewResult;
-                var userEditModel = viewResult?.Model as UserEditModel;
-
-                // assert
-                Assert.IsNotNull(result);
-                Assert.IsInstanceOfType(result, typeof(ViewResult));
-                Assert.IsNotNull(viewResult);
-                Assert.IsNotNull(viewResult.Model);
-                Assert.IsInstanceOfType(viewResult.Model, typeof(UserEditModel));
-                Assert.AreEqual(userEditModel.Id, userId);
-                Assert.AreEqual(userEditModel.Username, "Username");
-                Assert.AreEqual(userEditModel.Name, "Given");
-                Assert.AreEqual(userEditModel.Surname, "Sur");
-                Assert.AreEqual(userEditModel.Email, "email");
-                Assert.AreEqual(userEditModel.Roles, allRoles);
-                Assert.AreEqual(userEditModel.SelectedRoles, selectedRoles);
-            }
 
             private void SetupMinimalEnvironment(Guid userId)
             {
