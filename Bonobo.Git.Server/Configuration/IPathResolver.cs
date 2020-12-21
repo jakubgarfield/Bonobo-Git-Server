4d24d32d59aa17c8380afdd9dd2585f27e5af6d6
Eugenio Miró <eugenio.miro@gmail.com>
2020-12-21 20:04:04 -0300

    refactor ConfigurationEntry for testability

---
+++ Bonobo.Git.Server/Configuration/IPathResolver.cs
@@ -0,0 +1,8 @@
+﻿namespace Bonobo.Git.Server.Configuration
+{
+    public interface IPathResolver
+    {
+        string ResolveWithConfiguration(string configKey);
+        string Resolve(string path);
+    }
+}
