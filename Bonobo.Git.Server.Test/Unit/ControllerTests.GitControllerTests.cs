5c3b5f71687ec0b3b3a0011a47c0093540bd375b
Eugenio Miró <eugenio.miro@gmail.com>
2021-01-19 08:47:29 -0300

    add missing controller test classes

---
+++ Bonobo.Git.Server.Test/Unit/ControllerTests.GitControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Bonobo.Git.Server.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Bonobo.Git.Server.Test.Unit
+{
+    public partial class ControllerTests
+    {
+        [TestClass]
+        public class GitControllerTests : ControllerTests
+        {
+            [TestInitialize]
+            public void TestInitialize()
+            {
+                sut = new GitController();
+            }
+
+            // Get SecureGetInfoRefs tests
+            // Post SecureUploadPack
+            // Post SecureReceivePack
+            // Get GitUrl
+        }
+    }
+}
---
+++ Bonobo.Git.Server.Test/Unit/ControllerTests.ValidationControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Bonobo.Git.Server.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Bonobo.Git.Server.Test.Unit
+{
+    public partial class ControllerTests
+    {
+        [TestClass]
+        public class ValidationControllerTests : ControllerTests
+        {
+            [TestInitialize]
+            public void TestInitialize()
+            {
+                sut = new ValidationController();
+            }
+
+            // Get UniqueNameRepo
+            // Get UniqueNameUser
+            // Get UniqueNameTeam
+            // Get IsValidRegex
+        }
+    }
